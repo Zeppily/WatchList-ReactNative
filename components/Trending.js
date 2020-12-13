8921d0c672e71ed1e6f532a6fd785903c0ea3865
HiMyNameIsSunny <thaigrrs@gmail.com>
2020-12-14 00:05:44 +0200

    Trending Functionality

--- components/Trending.js
+++ components/Trending.js
@@ -1,48 +1,79 @@
-import React, {useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Firebase from "../config/firebase";
-import { useFocusEffect } from '@react-navigation/native';
+import { useFocusEffect } from "@react-navigation/native";
 import {
-    View,
-    TextInput,
-    StyleSheet,
-    TouchableOpacity,
-    Text,
-    ImageBackground,
-  } from "react-native";
-  import {
-    M_API_KEY,
-  } from "react-native-dotenv";
+  View,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  ImageBackground,
+  FlatList,
+  Image,
+  StatusBar
+} from "react-native";
+import { M_API_KEY } from "react-native-dotenv";
 
 export default function Trending({ navigation }) {
-
-      // If no user logged in => Login page
+  const [movies, setMovies] = useState([]);
+  // If no user logged in => Login page
   useFocusEffect(() => {
     const user = Firebase.auth().currentUser;
-    if(!user){
-        navigation.navigate("Login");
+    if (!user) {
+      navigation.navigate("Login");
     }
-},[]);
+  }, []);
 
-    useEffect(() => {
-        //fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${M_API_KEY}`)
-        //Flatlist with text under image and button to add to watchlist or onlongpress
-    }, []);
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${M_API_KEY}`)
+      .then((response) => response.json())
+      .then((responseData) => setMovies(responseData.results))
+      .catch((err) => console.log(err));
+  }, []);
 
-
+  const listSeparator = () => {
     return (
-        <View style={styles.container}>
-            <Text>Trending Page</Text>
-        </View>
-        
+      <View
+        style={{
+          marginTop: "3%",
+          width: "80%",
+          backgroundColor: "#fffff",
+          marginLeft: "10%",
+        }}
+      />
     );
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        keyExtractor={(item) => item.title}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+            <Image
+              source={{
+                uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+              }}
+              style={{ width: 300, height: 300 }}
+            />
+            <Text>{item.overview}</Text>
+          </View>
+        )}
+        ItemSeparatorComponent={listSeparator}
+        data={movies}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-  });
-  
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  flatlist: {},
+});
